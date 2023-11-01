4a58cb353ea7e2ca158bb76196ca5f23e4859566
MarcGriese <marcgriese76@gmail.com>
2023-11-01 23:18:59 +0100

    footer jsx layout

---
+++ PennyPlanner/src/components/footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {Link, useNavigate } from 'react-router-dom'
+import '../assets/css files/footer.jsx'
+
+
+export default function Footer() {
+    return(
+        <div className="footer_container">
+            <div className="upper-footer_container">
+                <div className="footer-logo-text_container">
+
+                </div>
+                <div className="footer-help-legal-socials_container">
+                    <div className="footer-help_container">
+
+                    </div>
+                    <div className="footer-legal_container">
+
+                    </div>
+                    <div className="footer-socials_container">
+
+                    </div>
+                </div>
+            </div>
+            <div className="lower-footer_container">
+                <div className="footer-copyright_container">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
