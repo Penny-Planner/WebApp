0fb2ecad1ff104aef06895556beea01bc84a67b6
MarcGriese <marcgriese76@gmail.com>
2023-11-02 15:19:05 +0100

    adjusted and added homepage styling

--- PennyPlanner/src/pages/Home/index.js
+++ PennyPlanner/src/pages/Home/index.js
@@ -6,8 +6,19 @@ import Footer from '../../components/footer.jsx'
 
 export default function Home() {
     return(
-        <div className="fullscreen">
+        <div className="fullscreen-home">
             <Header/>
+            <div className="home-main-slogan_container">
+                <div className="home-slogan-1_container andantetext-regular-black-40px">
+                    Manage all your expenses
+                </div>
+                <div className="home-slogan-2_container andantedisplay-bold-black-100px">
+                    See all your finances <br/> at a glance
+                </div>
+            </div>
+            <div className="much">
+
+            </div>
             <Footer />
         </div>
     );
