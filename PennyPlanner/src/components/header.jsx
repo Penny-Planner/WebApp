efd7115ffa7a629c796a7c113707c00dc7d19b07
MarcGriese <marcgriese76@gmail.com>
2023-11-02 15:18:34 +0100

    finished styling of footer and header

--- PennyPlanner/src/components/header.jsx
+++ PennyPlanner/src/components/header.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "../assets/css files/header.css"
+import {Link, useNavigate} from 'react-router-dom'
 
 export default function Header() {
-    return(
+    return (
         <div className="header_background">
-            <h1 className="pennyplanner_title">PennyPlanner</h1>
+            <div className="pennyplanner_title andantedisplay-bold-black-40px">
+                PennyPlanner
+            </div>
+            <div className="header-nav_container">
+                <Link to="" className="header-link_container andantetext-regular-black-40px">
+                    About Us
+                </Link>
+                <Link to="/signup" className="header-link_container andantetext-regular-black-40px">
+                    Sign Up
+                </Link>
+                <Link to="login" className="header-link_container andantetext-regular-black-40px">
+                    Log In
+                </Link>
+            </div>
         </div>
     );
 }
