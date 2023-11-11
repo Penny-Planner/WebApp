bfd2ed81aafc6c0c960408eb07ea9ae0813ed02a
MarcGriese <marcgriese76@gmail.com>
2023-11-11 21:20:21 +0100

    created about us routing + basic page

--- PennyPlanner/src/App.js
+++ PennyPlanner/src/App.js
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Profileview from './pages/Profile';
 import FAQ from './pages/FAQ';
+import AboutUs from './pages/AboutUs';
 
 const App = () => {
     return (
@@ -14,7 +15,7 @@ const App = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path='/signup' element={<Signup />} />
                 <Route path='/profile' element={<Profileview />} />
-
+                <Route path='/aboutus' element={<AboutUs />} />
                 <Route path='/faq' element={<FAQ />} />
 
 
---
+++ PennyPlanner/src/pages/AboutUs/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../components/header";
+import '../../assets/css files/aboutus.css'
+import Footer from '../../components/footer.jsx'
+import '../../assets/css files/global.css'
+
+
+export default function AboutUs() {
+    return(
+        <>
+            <Header/>
+            
+            <Footer />
+        </>
+    );
+}
