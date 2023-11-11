549f49fa8016fb4d594c89f6709151753f03a66d
MarcGriese <marcgriese76@gmail.com>
2023-11-11 12:18:21 +0100

    new build

--- PennyPlanner/deploy.sh
+++ PennyPlanner/deploy.sh
@@ -5,6 +5,6 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to server..."
-scp -r public/* marc@85.215.104.144:/var/www/85.215.104.144/
+scp -r public/* marc@85.215.61.94:/var/www/85.215.61.94/
 
 echo "Done!"
