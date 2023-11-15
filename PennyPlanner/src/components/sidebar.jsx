31d6e1d6b93f6018fcb2948be2b84fc6fd8bf4b0
MarcGriese <marcgriese76@gmail.com>
2023-11-16 00:29:35 +0100

    simplified sidebar component

--- PennyPlanner/src/components/sidebar.jsx
+++ PennyPlanner/src/components/sidebar.jsx
@@ -1,46 +1,55 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import "../assets/css files/sidebar.css"
-import PennyPlannerLogo from '../assets/images/PennyPlannerLogo.png'
-
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../assets/css files/sidebar.css";
+import PennyPlannerLogo from '../assets/images/PennyPlannerLogo.png';
 
 export default function Sidebar() {
+    const [selectedOption, setSelectedOption] = useState("/user/profile");
+    const location = useLocation();
+
+    useEffect(() => {
+        setSelectedOption(location.pathname);
+    }, [location.pathname]);
+
+    function Sidebar_option({ link, label }) {
+        return (
+            <Link
+                to={link}
+                className={link === selectedOption ? "selected-option_container andantetext-regular-black-28px" : "option_container andantetext-regular-black-28px"}
+                onClick={() => setSelectedOption(link)}
+            >
+                {label}
+            </Link>
+        );
+    }
+
+    function Sidebar_option_border({ link, label }) {
+        return (
+            <Link
+                to={link}
+                className={link === selectedOption ? "selected-option_container_border andantetext-regular-black-28px" : "option_container_border andantetext-regular-black-28px"}
+                onClick={() => setSelectedOption(link)}
+            >
+                {label}
+            </Link>
+        );
+    }
+
     return (
-        <div className="sidebar_frame">
+        <div className="sidebar_frame col-3">
             <div className="logo_container">
                 <img src={PennyPlannerLogo} alt="PennyPlanner Logo" className="pennyplanner_logo" />
             </div>
-            <Sidebar_option_border link="" label="Profile Information" />
-            <Sidebar_option link="" label="Notifications" />
-            <Sidebar_option link="" label="Goals" />
-            <Sidebar_option link="" label="Connected Apps" />
-            <Sidebar_option link="" label="Categories" />
-            <Sidebar_option link="" label="Add Bank Account" />
-            <Sidebar_option link="" label="Configure Profile" />
-            <Sidebar_option link="" label="FAQ" />
-            <Sidebar_option link="" label="Settings" />
-            <Sidebar_option_border link="" label="Log Out" />
-
+            <Sidebar_option_border link="/user/profile" label="Profile Information" />
+            <Sidebar_option link="/user/notifications" label="Notifications" />
+            <Sidebar_option link="/user/goals" label="Goals" />
+            <Sidebar_option link="/user/apps" label="Connected Apps" />
+            <Sidebar_option link="/user/categories" label="Categories" />
+            <Sidebar_option link="/user/addbank" label="Add Bank Account" />
+            <Sidebar_option link="/user/configuration" label="Configure Profile" />
+            <Sidebar_option link="/user/faq" label="FAQ" />
+            <Sidebar_option link="/user/settings" label="Settings" />
+            <Sidebar_option_border link="/" label="Log Out" />
         </div>
-    )
-}
-
-function Sidebar_option_border({ link, label }) {
-    return (
-        <>
-            <Link to={link} className="option_container_border andantetext-regular-black-28px">
-                {label}
-            </Link>
-        </>
-    )
-}
-
-function Sidebar_option({ link, label }) {
-    return (
-        <>
-            <Link to={link} className="option_container andantetext-regular-black-28px">
-                {label}
-            </Link>
-        </>
-    )
+    );
 }
