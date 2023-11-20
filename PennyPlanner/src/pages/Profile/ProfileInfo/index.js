994e00e334db0efd7c7578c8da76135919f54e3b
MarcGriese <marcgriese76@gmail.com>
2023-11-20 13:20:03 +0100

    you can now edit profile info

--- PennyPlanner/src/pages/Profile/ProfileInfo/index.js
+++ PennyPlanner/src/pages/Profile/ProfileInfo/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useNavigate, Outlet } from 'react-router-dom'
 import '../../../assets/css files/profile.css'
 import Sidebar from '../../../components/sidebar'
@@ -27,10 +27,10 @@ export default function ProfileInfo() {
 
                         <div className="user-information_column_container">
 
-                            <UserInfoRow category="Name" variable="Deine Mum" />
-                            <UserInfoRow category="E-Mail" variable="Deine Mum" />
-                            <UserInfoRow category="Phone" variable="Deine Mum" />
-                            <UserInfoRow category="Password" variable="Deine Mum" />
+                            <UserInfoRow category="Name" type="text" variable="John Doe" />
+                            <UserInfoRow category="E-Mail" type="text" variable="johndoe@gmail.com" />
+                            <UserInfoRow category="Phone" type="text" variable="+44 1632 960817" />
+                            <UserInfoRow category="Password" type="password" variable="password" />
 
                             <div className="user-buttons_container">
                                 <button className="user-info-button andantetext-regular-white-28px">
@@ -49,18 +49,51 @@ export default function ProfileInfo() {
     )
 }
 
-function UserInfoRow({ category, variable }) {
+function UserInfoRow({ category, type, variable }) {
+    const [buttonText, setButtonText] = useState('CHANGE');
+    const [buttonColor, setButtonColor] = useState('red');
+    const [editable, setEditable] = useState(false);
+    const [inputValue, setInputValue] = useState(variable);
+
+
+    const handleClick = () => {
+        if (buttonText === 'CHANGE') {
+            setButtonText('SEND');
+            setButtonColor('green');
+        } else {
+            setButtonText('CHANGE');
+            setButtonColor('red')
+        }
+        setEditable(!editable);
+    }
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
     return (
         <div className="user-information_row_container">
             <div className="outer-user-information_row_container andantetext-regular-black-28px">
                 {category}
             </div>
-            <div className="inner-user-information_row_container andantetext-regular-black-28px">
-                {variable}
-            </div>
-            <div className="outer-user-information_row_container andantetext-regular-black-28px">
-                CHANGE
-            </div>
+            {editable ? (
+                <input
+                    type={type}
+                    className="inner-user-information_row_container andantetext-regular-black-28px"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                />
+            ) : (
+                <input
+                    type={type}
+                    className="inner-user-information_row_container andantetext-regular-black-28px readonly-input"
+                    value={inputValue}
+                    readOnly
+                />
+            )}
+            <button onClick={handleClick} className="outer-user-information_row_container andantetext-regular-black-28px" style={{ borderColor: buttonColor }}>
+                {buttonText}
+            </button>
         </div>
     )
 }
